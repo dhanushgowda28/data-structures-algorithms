b260946cbc0c310658748abacf2e49037c77662e
Dhanush Gowda <dhanushgowda092@gmail.com>
2025-05-18 14:03:46 +0000

    Custome linked list

--- custome-linked-list/CustomeLinkedListMain.java
+++ custome-linked-list/CustomeLinkedListMain.java
@@ -1,52 +1,86 @@
-public class CustomeLinkedListMain {
-    public static void main(String[] args) {
+// Node class
+class Node {
+    int data;
+    Node next;
 
-        CustomeLinkedList list = new CustomeLinkedList();
-        list.add(0);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-
-        list.print();
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     }
-
 }
 
-class CustomeLinkedList {
+// LinkedList class
+public class CustomeLinkedListMain {
+    Node head;
 
-    private Node head;
+    // Insert at the end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
 
-    public void add(int num) {
         if (head == null) {
-            head = new Node(num);
-            return ;
+            head = newNode;
+            return;
         }
+
         Node current = head;
         while (current.next != null) {
             current = current.next;
         }
-        current.next = new Node(num);
 
+        current.next = newNode;
     }
 
-    void print() {
+    // Insert at the beginning
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Delete a node by value
+    public void deleteNode(int key) {
+        Node current = head, prev = null;
+
+        // If head needs to be deleted
+        if (current != null && current.data == key) {
+            head = current.next;
+            return;
+        }
+
+        // Search for the node to be deleted
+        while (current != null && current.data != key) {
+            prev = current;
+            current = current.next;
+        }
+
+        // If key was not found
+        if (current == null) {
+            System.out.println("Key not found");
+            return;
+        }
+
+        // Unlink the node
+        prev.next = current.next;
+    }
+
+    // Display the list
+    public void display() {
         Node current = head;
         while (current != null) {
-            System.out.println(current.num);
+            System.out.print(current.data + " -> ");
             current = current.next;
-
         }
+        System.out.println("null");
     }
 
-}
-
-class Node {
-    int num;
-    Node next;
-
-    Node(int num) {
-        this.num = num;
-        this.next = null;
+    // Main method for testing
+    public static void main(String[] args) {
+        CustomeLinkedListMain ll = new CustomeLinkedListMain();
+        ll.insertAtEnd(10);
+        ll.insertAtEnd(20);
+        ll.insertAtBeginning(5);
+        ll.display();  // Output: 5 -> 10 -> 20 -> null
+        ll.deleteNode(10);
+        ll.display();  // Output: 5 -> 20 -> null
     }
 }
